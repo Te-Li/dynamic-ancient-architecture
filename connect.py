1e15b70c870da5b61e321ccdb38d3c278b6c10dc
LI TE <64113838+Te-Li@users.noreply.github.com>
2025-04-04 19:34:56 +0800

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,11 +1,10 @@
 from PIL import Image, ImageSequence
 
 def combine_gifs(gif_paths, output_path):
-    # 获取帧序列
+
     with Image.open(gif_paths[0]) as im:
         frames = [frame.copy() for frame in ImageSequence.Iterator(im)]
 
-    # 遍历后续的 GIF 文件
     for path in gif_paths[1:]:
         with Image.open(path) as im:
 
@@ -13,6 +12,6 @@ def combine_gifs(gif_paths, output_path):
 
     frames[0].save(output_path, save_all=True, append_images=frames[1:], duration=im.info['duration'], loop=0)
 
-gif_paths = ['2.gif', '4.gif','5.gif']  # 替换为你的 GIF 文件路径
-output_path = 'combined.gif'  # 输出文件路径
+gif_paths = ['2.gif', '4.gif','5.gif'] 
+output_path = 'combined.gif' 
 combine_gifs(gif_paths, output_path)
